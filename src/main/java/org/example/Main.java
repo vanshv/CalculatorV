eabe15719d8f2d92dcbbdd2e6a879e28350358ae
vanshv <vanshvardhan.singh@iiitb.ac.in>
2023-11-06 23:43:07 +0530

    test webhook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("jar build test");
+        System.out.println("webhook test");
     }
 }
